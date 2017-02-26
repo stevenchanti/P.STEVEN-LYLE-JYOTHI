8455af258cda85046252b5120e1bfab0a0402291
stevenchanti <stevenchanti.y2k@gmail.com>
2017-02-26 09:30:12 -0800

    Add files via upload

---
+++ NODEMCU_ESP8266-12E.ino
@@ -0,0 +1,32 @@
+
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "your autho token";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "youretworkssd";
+char pass[] = "your network password";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 8442);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8442);
+}
+
+void loop()
+{
+  Blynk.run();
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+}
